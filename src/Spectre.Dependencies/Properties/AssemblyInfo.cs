c894894bd5f1f57d5ad76ec4f094af7b8e5bc600
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2017-12-16 14:53:46 +0100

    Annotate the assemblies

--- src/Spectre.Dependencies/Properties/AssemblyInfo.cs
+++ src/Spectre.Dependencies/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spectre.Dependencies")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Temporary module for handling Inversion of Control in the Spectre tool.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Spectre Team")]
 [assembly: AssemblyProduct("Spectre.Dependencies")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Spectre")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
