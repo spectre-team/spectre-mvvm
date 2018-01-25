0702701c0c92b4cb8ab0e33a1ce2ec554bc15db3
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-26 00:18:41 +0100

    Fix nuget restore

--- restore_nugets.bat
+++ restore_nugets.bat
@@ -1,5 +1,4 @@
 @echo off
-cd src
 nuget sources add -Name GMrukwaAppVeyorFeed -Source https://ci.appveyor.com/nuget/gmrukwa-xfn7vhwq20u6 -UserName %nuget_user% -Password %nuget_password%
 if errorlevel 1 (
 	echo Connection to GMrukwaAppVeyorFeed failed.
@@ -10,4 +9,3 @@ if errorlevel 1 (
 	echo NuGet failed to restore packages.
 	EXIT /B 1
 )
-cd ..
